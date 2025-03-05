67805c35281263c9e8d2b77a9c0e28b1576b9ab7
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-03-05 10:27:42 -0300

    Capitulo 1 Parte 4

---
+++ apps/api/src/index.ts
@@ -0,0 +1,10 @@
+import { ability } from '@projeto-react-08/auth'
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+
+console.log(userCanInviteSomeoneElse)
+console.log(userCanDeleteOtherUsers)
+console.log(userCannotDeleteOtherUsers)
