549a296222fbfd533e0f1ffd31548ca1ea579be3
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-03-15 12:11:46 -0300

    Capitulo 2 Parte 16

---
+++ apps/api/src/http/routes/orgs/get-organizations.ts
@@ -0,0 +1,71 @@
+import { roleSchema } from '@projeto-react-08/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+export async function getOrganizations(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get organizations where user is a member',
+          security: [{ bearerAuth: [] }],
+          response: {
+            201: z.object({
+              organizations: z.array(
+                z.object({
+                  id: z.string().cuid(),
+                  name: z.string(),
+                  slug: z.string(),
+                  avatarUrl: z.string().url().nullable(),
+                  role: roleSchema,
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (req) => {
+        const userId = await req.getCurrentUserId()
+
+        const organizations = await prisma.organization.findMany({
+          select: {
+            id: true,
+            name: true,
+            slug: true,
+            avatarUrl: true,
+            members: {
+              select: {
+                role: true,
+              },
+              where: {
+                userId,
+              },
+            },
+          },
+          where: {
+            members: {
+              some: {
+                userId,
+              },
+            },
+          },
+        })
+
+        const organizationsWithRole = organizations.map(
+          ({ members, ...org }) => {
+            return { ...org, role: members[0].role }
+          },
+        )
+
+        return { organizations: organizationsWithRole }
+      },
+    )
+}
