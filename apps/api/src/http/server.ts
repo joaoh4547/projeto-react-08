b4432d987e069e74884e460aab8b11c7bd482f4d
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-03-15 14:40:41 -0300

    Capitulo 2 Parte 18

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -23,6 +23,7 @@ import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizations'
 import { shutdownOrganization } from './routes/orgs/shutdown-organization'
+import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -70,6 +71,7 @@ app.register(resetPassword)
 app.register(createOrganization)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
+app.register(transferOrganization)
 app.register(getOrganizations)
 app.register(getOrganization)
 app.register(getMembership)
