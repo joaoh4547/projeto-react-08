2122a1b4b71987c435cc859843dd7a8837bc3748
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-03-23 17:46:17 -0300

    Capitulo 2 Parte 35

--- apps/api/src/http/routes/auth/reset-password.ts
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -36,10 +36,13 @@ export async function resetPassword(app: FastifyInstance) {
 
       const passwordHash = await hash(password, 8)
 
-      await prisma.user.update({
-        where: { id: tokenFromCode.userId },
-        data: { passwordHash },
-      })
+      await prisma.$transaction([
+        prisma.user.update({
+          where: { id: tokenFromCode.userId },
+          data: { passwordHash },
+        }),
+        prisma.token.delete({ where: { id: code } }),
+      ])
 
       return reply.status(204).send()
     },
