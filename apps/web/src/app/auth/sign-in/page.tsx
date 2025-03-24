2549dea12ca98e9cca34f49f407266ba1d503694
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-03-23 21:29:03 -0300

    Capitulo 3 Parte 1

---
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import githubIcon from '@/assets/github-icon.svg'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
+export default function SignInPage() {
+  return (
+    <form className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input name="email" type="email" id="email" />
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="password">Password</Label>
+        <Input name="password" type="password" id="password" />
+        <Link
+          href="/auth/forgot-password"
+          className="text-foreground text-xs font-medium hover:underline"
+        >
+          Forgot your password?
+        </Link>
+      </div>
+      <Button className="w-full" type="submit">
+        Sign In with e-mail
+      </Button>
+      <Separator />
+      <Button variant="outline" className="w-full" type="submit">
+        <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
+        Sign In with GitHub
+      </Button>
+    </form>
+  )
+}
