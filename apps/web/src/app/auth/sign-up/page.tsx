a4909a19576600321a0a6e9fb40109e32fdf3dc6
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-03-23 21:44:53 -0300

    Capitulo 3 Parte 2

--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -7,9 +7,13 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <form className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="name">Name</Label>
+        <Input name="name" id="name" />
+      </div>
       <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input name="email" type="email" id="email" />
@@ -17,24 +21,25 @@ export default function SignInPage() {
       <div className="space-y-1">
         <Label htmlFor="password">Password</Label>
         <Input name="password" type="password" id="password" />
-        <Link
-          href="forgot-password"
-          className="text-foreground text-xs font-medium hover:underline"
-        >
-          Forgot your password?
-        </Link>
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="password_confirmation">Confirm your password</Label>
+        <Input
+          name="password_confirmation"
+          type="password"
+          id="password_confirmation"
+        />
       </div>
       <Button className="w-full" type="submit">
-        Sign In with e-mail
+        Create Account
       </Button>
-
       <Button variant="link" className="w-full" size="sm" asChild>
-        <Link href="sign-up">Create new account</Link>
+        <Link href="sign-in">Already registered? Sign in</Link>
       </Button>
       <Separator />
       <Button variant="outline" className="w-full" type="submit">
         <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
-        Sign In with GitHub
+        Sign up with GitHub
       </Button>
     </form>
   )
