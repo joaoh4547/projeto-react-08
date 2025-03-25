34dfa35325dd636c1cca78f8287b23135fe3693f
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-03-25 20:03:39 -0300

    Capitulo 3 Parte 10

---
+++ apps/web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,31 @@
+import { cookies } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
+
+import { signInWithGitHub } from '@/http/sign-in-with-github'
+
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams
+
+  const code = searchParams.get('code')
+
+  if (!code) {
+    return NextResponse.json(
+      {
+        message: 'GitHub Oauth code was not found',
+      },
+      { status: 400 },
+    )
+  }
+
+  const { token } = await signInWithGitHub({ code })
+
+  const cookiesStore = await cookies()
+  cookiesStore.set('token', token, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
+  })
+
+  const redirectUrl = req.nextUrl.clone()
+  redirectUrl.pathname = '/'
+  return NextResponse.redirect(redirectUrl)
+}
--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { Label } from '@radix-ui/react-label'
-import { Separator } from '@radix-ui/react-separator'
 import { AlertTriangle, Loader2 } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -11,8 +10,10 @@ import githubIcon from '@/assets/github-icon.svg'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Separator } from '@/components/ui/separator'
 import { useFormState } from '@/hooks/use-form-state'
 
+import { signInWithGithub } from '../actions'
 import { signInWithEmailAndPassword } from './actions'
 
 export function SignInForm() {
@@ -31,56 +32,60 @@ export function SignInForm() {
   )
 
   return (
-    <form onSubmit={handleSignIn} className="space-y-4">
-      {!success && message && (
-        <Alert variant="destructive">
-          <AlertTriangle className="size-4" />
-          <AlertTitle>Sign In failed!</AlertTitle>
-          <AlertDescription>
-            <p>{message}</p>
-          </AlertDescription>
-        </Alert>
-      )}
-      <div className="space-y-1">
-        <Label htmlFor="email">E-mail</Label>
-        <Input name="email" type="text" id="email" />
-        {errors?.email && (
-          <p className="text-xs font-medium text-red-500 dark:text-red-400">
-            {errors.email[0]}
-          </p>
+    <div className="space-y-4">
+      <form onSubmit={handleSignIn} className="space-y-4">
+        {!success && message && (
+          <Alert variant="destructive">
+            <AlertTriangle className="size-4" />
+            <AlertTitle>Sign In failed!</AlertTitle>
+            <AlertDescription>
+              <p>{message}</p>
+            </AlertDescription>
+          </Alert>
         )}
-      </div>
-      <div className="space-y-1">
-        <Label htmlFor="password">Password</Label>
-        <Input name="password" type="password" id="password" />
-        {errors?.password && (
-          <p className="text-xs font-medium text-red-500 dark:text-red-400">
-            {errors.password[0]}
-          </p>
-        )}
-        <Link
-          href="forgot-password"
-          className="text-foreground text-xs font-medium hover:underline"
-        >
-          Forgot your password?
-        </Link>
-      </div>
-      <Button className="w-full" type="submit" disabled={isPending}>
-        {isPending ? (
-          <Loader2 className="size-4 animate-spin" />
-        ) : (
-          'Sign In with e-mail'
-        )}
-      </Button>
+        <div className="space-y-1">
+          <Label htmlFor="email">E-mail</Label>
+          <Input name="email" type="text" id="email" />
+          {errors?.email && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.email[0]}
+            </p>
+          )}
+        </div>
+        <div className="space-y-1">
+          <Label htmlFor="password">Password</Label>
+          <Input name="password" type="password" id="password" />
+          {errors?.password && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.password[0]}
+            </p>
+          )}
+          <Link
+            href="forgot-password"
+            className="text-foreground text-xs font-medium hover:underline"
+          >
+            Forgot your password?
+          </Link>
+        </div>
+        <Button className="w-full" type="submit" disabled={isPending}>
+          {isPending ? (
+            <Loader2 className="size-4 animate-spin" />
+          ) : (
+            'Sign In with e-mail'
+          )}
+        </Button>
 
-      <Button variant="link" className="w-full" size="sm" asChild>
-        <Link href="sign-up">Create new account</Link>
-      </Button>
+        <Button variant="link" className="w-full" size="sm" asChild>
+          <Link href="sign-up">Create new account</Link>
+        </Button>
+      </form>
       <Separator />
-      <Button variant="outline" className="w-full" type="submit">
-        <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
-        Sign In with GitHub
-      </Button>
-    </form>
+      <form action={signInWithGithub}>
+        <Button variant="outline" className="w-full" type="submit">
+          <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
+          Sign In with GitHub
+        </Button>
+      </form>
+    </div>
   )
 }
